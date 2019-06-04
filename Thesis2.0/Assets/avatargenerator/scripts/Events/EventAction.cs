3d1fb782dfbb4dd2042296b8b9817d314f08ecd3
DESKTOP-E8OEJA7\schnu <markus-ws-weiss@outlook.de>
2019-06-04 17:36:01 +0200

    Delete

--- Thesis2.0/Assets/avatargenerator/scripts/Events/EventAction.cs
+++
@@ -1,5 +0,0 @@
-﻿﻿﻿﻿﻿﻿﻿﻿public class EventAction : GameAction
-{
-    public bool IsEvent = true;
-}
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
--- Thesis2.0/Assets/avatargenerator/scripts/Extensions/LoopExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿﻿﻿﻿﻿﻿﻿﻿public static class LoopExtensions
-{
-    public static void Times(this int count, System.Action action)
-    {
-        for (var i = 0; i < count; i++)
-            action();
-    }
-}
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
